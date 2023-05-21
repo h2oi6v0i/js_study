5be30554312a5f3fca589cbeb83fe5abf543e36f
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-05-21 19:21:12 +0900

    [86][monster-killer] : 강한 공격

--- 03MonsterKiller/assets/scripts/app.js
+++ 03MonsterKiller/assets/scripts/app.js
@@ -3,6 +3,7 @@
  * 내가 하드코딩한 이 코드가 전역 값임을 명시하기 위해 대문자로 작성
  */
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 
 /** 몬스터의 최대 공격력 */
 const MONSTER_ATTACK_VALUE = 14;
@@ -18,25 +19,51 @@ let currentPlayerHealth = chosenMaxLife;
 adjustHealthBars( chosenMaxLife );
 
 /** 
- * 몬스터 공격
+ * 몬스터 데미지 수치
+ * 이벤트 핸들러에 직접 연결하지 않기 때문에 핸들러라는 키워드 쓰지 않음
+ * attackHandler(), strongAttackHandler()에서 직접 호출하는 함수
  */
-function attackHandler() {
-    /** 몬스터 데미지 */
-    const damage = dealMonsterDamage( ATTACK_VALUE );
+function attackMonster( mode )  {
+    /** 얘 왜 만듦? */
+    let maxDamage;
+
+    if ( mode === 'ATTACK' ) {
+        maxDamage = ATTACK_VALUE;
+    } else if ( mode === 'STRONG_ATTACK' ) {
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+
+    /** 몬스터 데미지 (일반 공격 / 강한 공격)*/
+    const damage = dealMonsterDamage( maxDamage );
     currentMonsterHealth -= damage;
 
     /** 플레이어 데미지 */
     const playerDamage = dealPlayerDamage( MONSTER_ATTACK_VALUE );
-    currentPlayerHealth -= playerDamage;   
+    currentPlayerHealth -= playerDamage;
 
     /** 승리 기준 */
-    if ( currentMonsterHealth <= 0 && currentPlayerHealth > 0 ) {
-        alert( 'You Won!' );
+    if ( currentMonsterHealth <= 0 && currentPlayerHealth > 0 )  {
+        alert( 'You Won!');
     } else if ( currentPlayerHealth <= 0 && currentMonsterHealth > 0 ) {
-        alert( 'You Lost!' );
+        alert( 'You Lost!');
     } else if ( currentMonsterHealth <= 0 && currentPlayerHealth <= 0 ) {
-        alert( 'You have a draw!' );
+        alert( 'You have a draw!');
     }
 }
 
-attackBtn.addEventListener( 'click', attackHandler );
+/** 
+ * ✨ 몬스터 공격 핸들러
+ */
+function attackHandler() {
+    attackMonster( 'ATTACK' );
+}
+
+/** 
+ * ✨ 더 강한 몬스터 공격 핸들러
+ */
+function strongAttackHandler() {
+    attackMonster( 'STRONG_ATTACK' );
+}
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
